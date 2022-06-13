aef3b50fdf6fbca3a2b6bd04aa59bc90089c9227
ygz <350926623@qq.com>
2022-06-13 18:08:57 +0800

    配置代理

---
+++ src/setupProxy.js
@@ -0,0 +1,24 @@
+/**
+ * 配置代理 npm install http-proxy-middleware --save
+ * Note: You do not need to import this file anywhere. It is automatically registered when you start the development server.
+ */
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  // 代理信息
+  const proxyInfos = {
+    '/dzapi': {
+      // 触发部署////
+      target: 'http://192.168.0.241:8080',
+      // target: 'http://127.0.0.1:7001',
+      changeOrigin: true
+    },
+    '/app': {
+      target: 'http://120.92.92.218:8080/',
+      changeOrigin: true
+    }
+  };
+  Object.entries(proxyInfos).forEach(([key, proxyInfo]) => {
+    app.use(createProxyMiddleware(key, proxyInfo));
+  });
+};
