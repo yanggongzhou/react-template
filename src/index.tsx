6f37506118f7157b09bbfef1831549e0fc5233c9
ygz <350926623@qq.com>
2022-06-13 16:37:28 +0800

    eslint 环境配置

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './app';
 import reportWebVitals from './reportWebVitals';
 
+const dom = document.getElementById('root');
+
+console.log(dom);
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  dom as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
